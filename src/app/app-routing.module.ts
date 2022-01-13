f4b81a27fa99b3cd9a5960313d471dd311f9d184
AmitSisodiya275 <amitsisodiya275@gmail.com>
2022-01-13 09:12:53 +0530

    Added: Completed header component and added dashboard component and added routing for the dashboard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/persons', component: DashboardComponent},
+  {path:'', redirectTo:'/person', pathMatch:"full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
